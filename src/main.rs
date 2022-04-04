16434920b30016c86b57889250b5ef2a77eb1b46
Noah Beach <47062198+noahwbeach@users.noreply.github.com>
2022-04-03 20:23:27 -0500

    Updated port to 80

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
-    println!("Started server on port 8080");
+    let listener = TcpListener::bind("127.0.0.1:80")?;
+    println!("Started server on port 80");
 
     for stream in listener.incoming() {
         handle_client(stream?);
